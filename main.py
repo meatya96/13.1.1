ea3ce5d970796c0de084f8ff3a7890202f375630
Meatya <meatya96@gmail.com>
2024-03-05 21:18:12 +0600

    added .gitignore products amount class attribute

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ class Category():
     description: str
     products: list
     category_amount = 0
+    products_amount = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
         set_products = set(products)
-        self.products_amount = len(set_products)
+        self.__class__.products_amount += len(set_products)
         self.__class__.category_amount += 1
 
 class Product():
