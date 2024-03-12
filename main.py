94c255e55f0ab0f9e528e0bf19b8c5eef45dca87
Meatya <meatya96@gmail.com>
2024-03-12 20:36:14 +0600

    13.2

--- main.py
+++ main.py
@@ -1,26 +1,71 @@
 class Category():
     name: str
     description: str
-    products: list
+    _products: list
     category_amount = 0
     products_amount = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
-        set_products = set(products)
+        self._products = products
+        products_name = []
+        for item in self._products:
+            products_name.append(item['name'])
+        set_products = set(products_name)
         self.__class__.products_amount += len(set_products)
         self.__class__.category_amount += 1
 
+    def add_product(self, product):
+        self._products.append(product)
+
+    @property
+    def products(self):
+        product_list = []
+        for product in self._products:
+            product_list.append(f"{product['name']}, {product['price']} руб. Остаток: {product['amount']} шт.")
+        return product_list
+
+data_category = {'name': 'Шоколад',
+                     'description': 'Сладкий',
+                     'products': [{'name': 'mars',
+                    'description': 'Очень вкусный',
+                    'price': 50,
+                    'amount': 10},
+                    {'name': 'bounty',
+                    'description': 'Очень вкусный',
+                    'price': 55,
+                    'amount': 7},
+                    {'name': 'snickers',
+                    'description': 'Очень вкусный',
+                    'price': 60,
+                    'amount': 12}]}
+category_1 = Category(data_category['name'], data_category['description'], data_category['products'])
+
+print(category_1.products)
 class Product():
     name: str
     description: str
-    price: float
+    _price: float
     amount: int
 
     def __init__(self, name, description, price, amount):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.amount = amount
+
+    @staticmethod
+    def create_product(name, description, price, amount):
+        return Product(name, description, price, amount)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Ошибка: Цена должна быть больше нуля.")
+        else:
+            self._price = value
--- test_main.py
+++ test_main.py
@@ -2,16 +2,27 @@ from main import Product, Category
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def category_test():
     data_category = {'name': 'Шоколад',
                      'description': 'Сладкий',
-                     'products': ['mars', 'snickers', 'bounty']}
+                     'products': [{'name': 'mars',
+                    'description': 'Очень вкусный',
+                    'price': 50,
+                    'amount': 10},
+                    {'name': 'bounty',
+                    'description': 'Очень вкусный',
+                    'price': 55,
+                    'amount': 7},
+                    {'name': 'snickers',
+                    'description': 'Очень вкусный',
+                    'price': 60,
+                    'amount': 12}]}
 
     return Category(data_category['name'], data_category['description'], data_category['products'])
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def product_test():
     data_product = {'name': 'mars',
                     'description': 'Очень вкусный',
@@ -21,11 +32,11 @@ def product_test():
 
 
 def test_products_amount(category_test):
-    expected_products_amount = 3
+    expected_products_amount = 6
     assert category_test.products_amount == expected_products_amount
 
 def test_category_amount(category_test):
-    expected_category_amount = 2
+    expected_category_amount = 3
     assert category_test.category_amount == expected_category_amount
 
 
@@ -40,7 +51,7 @@ def test_category_description(category_test):
 
 
 def test_category_products(category_test):
-    expected_item = ['mars', 'snickers', 'bounty']
+    expected_item = ['mars, 50 руб. Остаток: 10 шт.', 'bounty, 55 руб. Остаток: 7 шт.', 'snickers, 60 руб. Остаток: 12 шт.']
     assert category_test.products == expected_item
 
 
